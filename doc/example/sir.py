e963efa5fa81267da0596229d08fe6c41c6a85d3
yannikschaelte <yannik.schaelte@gmail.com>
2019-03-05 21:33:27 +0100

    add sir example

---
+++ doc/example/sir.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.insert(0, '/home/yannik/ssa')
+
+import ssa
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def run():
+    reactants = np.array([[1, 1, 0], [0, 1, 0]])
+    products = np.array([[0, 2, 0], [0, 0, 1]])
+    x0 = np.array([299, 1, 0])
+    t_max = 1.0
+    k = np.array([0.3, 5])
+    x_names = ["Susceptible", "Infected", "Recovered"]
+    model = ssa.Model(reactants, products, x0=x0, t_max=t_max, k=k)
+    result = model.simulate(n_reps=20)
+    ssa.plot(result, x_names=x_names, set_xlim_to_t_max=False, show=False)
+    plt.savefig("sir.png")
+
+
+run()
--- doc/example/two_state_gene_expression.py
+++ doc/example/two_state_gene_expression.py
@@ -3,13 +3,15 @@ sys.path.insert(0, '/home/yannik/ssa')
 
 import ssa
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def run():
+    # activation, deactivation, transcription-act, transcription-deact, translation, mRNA decay, protein decay
     reactants = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
     products = np.array([[0, 1, 0, 0], [1, 0, 0, 0], [0, 1, 1, 0], [1, 0, 1, 0], [0, 0, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]])
 
-    k = np.array([10, 10, 0.5, 0.05, 10, 1, 0.2])
+    k = np.array([0.5, 0.5, 20, 0.0001, 15, 1, 2])
     x0 = np.array([1, 0, 0, 0])
     t_max = 30
 
@@ -18,8 +20,9 @@ def run():
     x_names = ["inactive promoter", "active promoeter", "mRNA", "protein"]
 
     model = ssa.Model(reactants=reactants, products=products, k=k, x0=x0, t_max=t_max, output=output)
-    result = model.simulate(n_reps=100)
-    ssa.plot(result, x_names=x_names, show=True)
+    result = model.simulate(n_reps=1)
+    ssa.plot(result, x_names=x_names, show=False)
+    plt.savefig("two_state_gene_expression.png")
 
 
 run()
